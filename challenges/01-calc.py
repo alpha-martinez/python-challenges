955334c1e33534d376dc9b851dae3f9388bda4be
alpha-martinez <martinezalphaa@gmail.com>
2020-09-14 14:22:49 -0400

    got first challenege done

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -3,26 +3,20 @@
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
 
-def add_numbers(num1, num2):
-    result = num1 + num2
-    return result
-
-def subtract_numbers(num1, num2):
-    result = num1 - num2
-    return result
-
-def multiply_numbers(num1, num2):
-    result = num1 * num2
-    return result
-
-def divide_numbers(num1, num2):
-    result = num1 / num2
-    return result  
-
-
 calc = input('What calculation would you like to do? (add, sub, mult, div)')
 num1 = int(input("1st number?"))
 num2 = int(input("2nd Number?"))
 
 if calc == "add":
-    print(add_numbers)
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif calc == "sub": 
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif calc == "mult":
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+else:
+    calc == "div"
+    result = num1 / num2
+    print(f"{num1} / {num2} = {result}")
