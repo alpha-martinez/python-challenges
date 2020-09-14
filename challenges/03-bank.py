21226ab52470afd1f65f3d7928aa0568df7b8f38
alpha-martinez <martinezalphaa@gmail.com>
2020-09-14 16:45:44 -0400

    revisit this later. cant seem to get my function for banking correct

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,31 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
 
+balance = "7000"
+
+def banking(balance):
+    print('Your current balance is')
+    print('$' + balance)
+    first = input("What would you like to do? (deposit, withdraw, check_balance)")
+
+    if first == "deposit":
+        deposit = input('How much would you like to deposit? ')
+        print('Deposited $' + int(deposit))
+        balance = int(deposit) + int(balance)
+    elif first == "withdraw":
+        withdraw = input('How much would you like to withdraw from your account? ')
+        print ('Withdrew $' + withdraw)
+        balance = int(balance) - int(withdraw)    
+    elif action == "check_balance":
+        balance == balance
+    else:     
+        print('Sorry')
+
+    done = input('Are you done? (yes or no)')
+    if done == "yes":
+        print('Have a nice day!')
+    else: 
+        print('What would you like to do next? (deposit, withdraw, check_balance)') 
+        
+banking(balance)
+
+print("Have a nice day!")   
